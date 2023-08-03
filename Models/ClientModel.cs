6399703b02484733271d924a1174e516620d6464
horacio3000 <127566294+horacio3000@users.noreply.github.com>
2023-08-04 00:59:45 +0300

    q

---
+++ Models/ClientModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TESTDB1.Models
+{
+    public class ClientModel
+    {
+        public Int64 idzeut { get; set; }
+        public string? cliname { get; set; }
+        public bool isMale { get; set; }
+        public int productType { get; set; }
+        public DateTime dDate { get; set; }
+      
+
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TESTDB1.Models
+{
+    public class ProductModel
+    {
+        public int productid { get; set; }
+        public string? prname { get; set; }
+        public int price { get; set; }
+        public int productType { get; set; }
+        public string? thumb { get; set; }
+
+
+    }
+
+
+
+
+    //productid,price,productType,thumb,prname
+}
